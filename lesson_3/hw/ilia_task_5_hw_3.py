a0216e417b0b5d3de58f4c39044e7ed000117032
Ilia Kasianov <i.kasyanov@cherryhome.ai>
2021-11-08 10:02:13 +0200

    fixed task 5 hw 3

--- lesson_3/hw/ilia_task_5_hw_3.py
+++ lesson_3/hw/ilia_task_5_hw_3.py
@@ -1,4 +1,13 @@
+'''
+Первая версия
 user_input = input("Please enter a sentence: ")
 user_input = user_input.lower()
 user_input = user_input.replace('черт', '####')
 print(user_input)
+'''
+
+# Вторая и рабочая версия
+import re
+user_input = input("Please enter a sentence: ")
+replaced_input = re.sub(r'(Ч|ч)(Е|е)(Р|р)(Т|т)\s', '#### ', user_input)
+print(replaced_input)
