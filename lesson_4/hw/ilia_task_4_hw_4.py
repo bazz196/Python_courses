655aeec36f3e76d519f0e6c38e38e7b371ff0917
Ilia Kasianov <i.kasyanov@cherryhome.ai>
2021-11-14 17:09:16 +0200

    hw_4

---
+++ lesson_4/hw/ilia_task_4_hw_4.py
@@ -0,0 +1,9 @@
+user_name = input()  # Получаем имя юзера
+upper_name, lower_name = user_name.upper(), user_name.lower()
+print('ИМЯ - {}, имя - {}'.format(upper_name, lower_name))
+
+'''
+Задача 4. 5 баллов
+Пользователь водит свое имя. Возвращается текст в БОЛЬШОМ и маленьком регистре. Использовать ' '.format().
+Для вставки аргументов в текст.
+'''
---
+++ lesson_4/hw/ilia_task_6_hw_4.py
@@ -0,0 +1,11 @@
+my_tuple = (1, 2, 3)
+first_elem, second_elem, *other_elements = my_tuple
+new_tuple = first_elem, second_elem
+print(new_tuple)
+'''
+Задача 6. 5 баллов
+Тема Кортеж и работа с ним.
+Удалить элемент из кортежа.
+Входные данные: (1, 2, 3)
+Результат: (1, 2)
+'''
